9e0838c4ebbc459f59c5017d3757519e7ed606eb
dwanjiku1-lgtm <d.wanjiku1@alustudent.com>
2026-06-03 14:51:24 -0700

    clinical analyst

---
+++ hospital_analysis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Member 5 - Clinical Analyst
+process_vitals() {
+    echo "Processing critical vitals..."
+    grep "CRITICAL" active_logs/heart_rate_log.txt active_logs/temperature_log.txt | \
+    awk '{print $1, $2, $3}' > reports/critical_alerts.txt
+    echo "Critical alerts saved to reports/critical_alerts.txt"
+}
+
