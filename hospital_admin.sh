113583164b7a0253e5f7544ea108675c89a044f2
Abigail-Cobbina <a.amaopooco@alustudent.com>
2026-06-04 08:11:15 -0700

    Add Member 3 orchestration logic

---
+++ hospital_admin.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+initialize_system
+secure_data
+
+echo ""
+echo "==========================================="
+echo " System Environment Secured"
+echo " Date: $(date)"
+echo "==========================================="
